bb4594035b4395bc1437a62b3b826a10cfc6475d
zeroheat0930 <dongjunkorea@naver.com>
2023-08-04 15:59:16 +0900

    버튼클릭 이벤트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Header(props){
   return (
@@ -16,10 +17,12 @@ function Nav(props) {
   const lis = []
   for(let i=0; i<props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={event =>{
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event =>{
       event.preventDefault();
-      props.onChangeMode(event.target.id);
-    }}>{t.title}</a></li>);
+      props.onChangeMode(Number(event.target.id));
+    }}>{t.title}</a>
+    </li>);
   }
   return (
     <nav>
@@ -41,20 +44,38 @@ function Article(props){
 
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, Web"></Article>
+  } else if (mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
     <div>
-      <Header title="WEB" onChangeMode={() => {
-        alert('Header');
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
       <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+        setMode('READ');
+        setId(id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, Web"></Article>
+      {content}
     </div>
   );
 }
